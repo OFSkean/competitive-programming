bb116bd3ceed07acfec9ecc2bee75eaaeeebc38e
Oscar Skean <ofskean727@gmail.com>
2022-02-08 20:52:15 -0500

    updated template

--- template.cpp
+++ template.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 #define ll long long
 #define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0) 
+#define pb push_back
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
 
 void output_vector(vector<int> v) {
 	for (auto i: v) {
