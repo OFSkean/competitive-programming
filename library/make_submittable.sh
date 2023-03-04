e6ec158c7405214b80df234549b345786db880c0
Oscar Skean <ofskean727@gmail.com>
2023-03-03 23:06:20 -0500

    updated submitify script to allow nested dependencies

--- library/make_submittable.sh
+++ library/make_submittable.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
 function recurse()
-{
-    ./make_submittable.sh $1 0
+{   
+    filename="$1"
+    shift
+    array=("$@")
+    
+    ./make_submittable.sh "../../library/$filename" "${array[@]}"
 }
-
-if [[ $# -eq 0 ]] ; then
-    echo 'need to supply a filename'
-        exit 1
-    fi
 
 newfilename=submittable.cpp
 
-if [[ $# != 2 ]]
+# make sure that filename to submitify is given
+if [[ $# -eq 0 ]] ; then
+    echo 'need to supply a filename'
+    exit 1
+fi
+
+currentfilename="$1"
+shift
+
+if [[ $# < 2 ]]
 then
     rm $newfilename
     touch $newfilename
 fi
 
-cat $1 | while read line
+arr=("$@")
+# make sure we havent already outputted the current file
+if [[ " ${arr[*]} " =~ " $currentfilename " ]]; then
+    echo "already added $currentfilename, exiting..."
+    exit 1
+fi
+
+echo "adding $currentfilename"
+arr+=("$currentfilename")
+
+cat $currentfilename | while read line
 do
     if [[ $line = \#include* ]]
     then
         filename=`echo $line | cut -d'"' -f 2`
-        if [[ $filename != *stdc\+\+* && $# != 2 ]]
+        echo $filename
+        if [[ ($filename != *stdc\+\+* && ! $filename =~ "template.h")  || ($filename =~ "template.h" && $# = 1) ]]
         then
-            recurse $filename
+            recurse $filename "${arr[@]}"
         fi
 
-        if [[ $filename = *stdc\+\+* && $# = 2 ]]
+        if [[ $filename = *stdc\+\+* && $currentfilename =~ "template.h" ]]
         then
             echo $line >> $newfilename
         fi
@@ -37,5 +56,10 @@ do
     fi
 done
 
-# make it readable
-vim -e -s -n "+normal gg=GZZ" $newfilename
+if [[ $# < 2 ]] 
+then
+    # make it readable
+    sed 's/\r$//' $newfilename > tmp.txt
+    mv tmp.txt $newfilename
+    vim -e -s -n "+normal gg=GZZ" $newfilename
+fi
